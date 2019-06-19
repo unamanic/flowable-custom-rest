bbbf3b73c27f3920114b599287c4e00a293907c5
William Witt <william@witt-family.net>
2019-06-19 11:26:11 -0500

    Initial commit

---
+++ src/main/java/com/example/flowablecustomrest/controllers/ProcessController.java
@@ -0,0 +1,52 @@
+package com.example.flowablecustomrest.controllers;
+
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
+import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.task.api.Task;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/rest-driven-process")
+public class ProcessController {
+
+    private final RuntimeService runtimeService;
+    private final TaskService taskService;
+
+
+    public ProcessController(RuntimeService runtimeService, TaskService taskService) {
+        this.runtimeService = runtimeService;
+        this.taskService = taskService;
+    }
+
+    @GetMapping("/start")
+    public String startProcess() {
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("restDrivenProcess");
+
+        return processInstance.getProcessInstanceId();
+    }
+
+    @GetMapping("/continue/{processInstanceId}")
+    public String continueProcess(@PathParam("processInstanceId") String processInstanceId) {
+        List<Task> tasks = taskService.createTaskQuery().taskDefinitionKey("secondRequest").processInstanceId(processInstanceId).list();
+
+        tasks.stream().forEach(task -> taskService.complete(task.getId()));
+
+        return tasks.size() + "complete";
+    }
+
+    @GetMapping("/movingOn/{processInstanceId}")
+    public List<String>  movingOn(@PathParam("processInstanceId") String processInstanceId) {
+        List<Task> tasks = taskService.createTaskQuery().taskDefinitionKey("otherTask").processInstanceId(processInstanceId).list();
+
+        return tasks.stream().map(Task::getId).collect(Collectors.toList());
+    }
+
+}
---
+++ src/test/java/com/example/flowablecustomrest/FlowableCustomRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.flowablecustomrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlowableCustomRestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
